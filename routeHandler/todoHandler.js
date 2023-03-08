3b502ef48df2d5c1208200fe99c8418fa1b7b0c5
shamsulhuda <shamsulhuda310@gmail.com>
2023-03-09 01:31:59 +0600

    CRUD Done

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -9,10 +9,41 @@ const todoSchema = require('../schemas/todoSchema');
 const Todo = new mongoose.model('Todo', todoSchema);
 
 // Get all TODOs
-router.get('/', async (req, res) => {});
+router.get('/', async (req, res) => {
+    await Todo.find({ status: 'inactive' })
+        .select({
+            _id: 0,
+            __v: 0,
+            date: 0,
+        })
+        .then((data) => {
+            res.status(200).json({
+                message: 'Data found!',
+                result: data,
+            });
+        })
+        .catch(() => {
+            res.status(500).json({
+                err: 'Something is wrong on server side!',
+            });
+        });
+});
 
 // Get a TODO
-router.get('/:id', async (req, res) => {});
+router.get('/:id', async (req, res) => {
+    await Todo.findOne({ _id: req.params.id })
+        .then((data) => {
+            res.status(200).json({
+                message: 'Data found!',
+                result: data,
+            });
+        })
+        .catch(() => {
+            res.status(500).json({
+                err: 'Something is wrong on server side!',
+            });
+        });
+});
 
 // Post a TODO
 router.post('/', async (req, res) => {
@@ -47,9 +78,45 @@ router.post('/all', async (req, res) => {
 });
 
 // Put a TODO
-router.put('/:id', async (req, res) => {});
+router.put('/:id', async (req, res) => {
+    await Todo.findByIdAndUpdate(
+        { _id: req.params.id },
+        {
+            $set: {
+                status: 'inactive',
+            },
+        },
+        {
+            new: true,
+            useFindAndModify: false,
+        }
+    )
+        .then((data) => {
+            res.status(200).json({
+                message: 'Todo updated successfully!',
+                result: data,
+            });
+        })
+        .catch(() => {
+            res.status(500).json({
+                err: 'Something is wrong on server side!',
+            });
+        });
+});
 
 // Delete a TODO
-router.delete('/:id', async (req, res) => {});
+router.delete('/:id', async (req, res) => {
+    await Todo.deleteOne({ _id: req.params.id })
+        .then(() => {
+            res.status(200).json({
+                message: 'Todo Deleted successfully!',
+            });
+        })
+        .catch(() => {
+            res.status(500).json({
+                err: 'Something is wrong on server side!',
+            });
+        });
+});
 
 module.exports = router;
