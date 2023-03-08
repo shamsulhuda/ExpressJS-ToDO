bebfa0eace4b56b8b9f0e6eb515df93ba3f9186c
shamsulhuda <shamsulhuda310@gmail.com>
2023-03-08 17:28:58 +0600

    Single Data insertion

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+// Get router
+const todoHandler = require('./routeHandler/todoHandler');
+
 // express app initialization
 const app = express();
 app.use(express.json());
 
 // Database connect with mongoose
+const username = 'shamsulhuda';
+const password = '92xxEt5giBzpxpgr';
+const cluster = 'cluster0.abrghjl';
+const dbname = 'test';
+
+mongoose
+    .connect(
+        `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    )
+    .then(() => {
+        console.log('Connection Success!');
+    })
+    .catch((err) => console.log(err));
 
 // application route
+app.use('/todo', todoHandler);
 
 // default error handler
 function errorHandler(err, req, res, next) {
---
+++ routeHandler/todoHandler.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const router = express.Router();
+
+const todoSchema = require('../schemas/todoSchema');
+
+// Create a model
+const Todo = new mongoose.model('Todo', todoSchema);
+
+// Get all TODOs
+router.get('/', async (req, res) => {});
+
+// Get a TODO
+router.get('/:id', async (req, res) => {});
+
+// Post a TODO
+router.post('/', async (req, res) => {
+    const newTodo = new Todo(req.body);
+    await newTodo
+        .save()
+        .then(() => {
+            res.status(200).json({
+                message: 'Todo created successfully!',
+            });
+        })
+        .catch(() => {
+            res.status(500).json({
+                err: 'Something is wrong on server side!',
+            });
+        });
+});
+
+// Post Multiple TODOs
+router.post('/all', async (req, res) => {});
+
+// Put a TODO
+router.put('/:id', async (req, res) => {});
+
+// Delete a TODO
+router.delete('/:id', async (req, res) => {});
+
+module.exports = router;
